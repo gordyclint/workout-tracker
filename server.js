23316e50593e6a49c09ee49d910785880ea7dec1
gordyclint <55668552+gordyclint@users.noreply.github.com>
2020-03-17 19:05:57 -0400

    Tweaking Heroku functionality

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 var PORT = process.env.PORT || 8808;
 var app = express();
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI);
 
 app.use(express.urlencoded({ extended: true}));
